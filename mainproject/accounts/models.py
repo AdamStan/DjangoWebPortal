df7bbfa0582db45a6eb1683ef5718b540bcfc42f
AdamStan <adam15stan@gmail.com>
2018-10-21 21:47:04 +0200

    What I've done...

--- mainproject/accounts/models.py
+++ mainproject/accounts/models.py
@@ -1,20 +1,45 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
+from entities.models import *
 # Create your models here.
-class User(AbstractUser):
-    is_student = models.BooleanField(default=False)
-    is_teacher = models.BooleanField(default=False)
-    is_scheduler = models.BooleanField(default=False)
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=64)
+    is_active = models.BooleanField(default=True)
+    name = models.CharField(max_length=32)
+    surname = models.CharField(max_length=64)
+    #types of users:
+    student = models.BooleanField(default=False)
+    teacher = models.BooleanField(default=False)
+    scheduler = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS =  ['name', 'surname']
+
+    def __str__(self):
+        return self.name + ', ' + self.surname
+
+    @property
+    def is_student(self):
+        return self.student
+
+    @property
+    def is_teacher(self):
+        return self.teacher
+
+    @property
+    def is_scheduler(self):
+        return self.scheduler
 
 class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=32)
-    surname = models.CharField(max_length=64)
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, primary_key=True, related_name='student_to_user')
+    #fieldOfStudy
+    yearOfStudy = models.DateTimeField()
+    #currentSubjects
 
-class Tutor(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-    name = models.CharField(max_length=32)
-    surname = models.CharField(max_length=64)
+class Teacher(models.Model):
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, primary_key=True, related_name='professor_to_user')
+    #faculty
+    #subjects
 
 class Scheduler(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField('accounts.User', on_delete=models.CASCADE, primary_key=True, related_name='scheduler_to_user')
--- mainproject/entities/models.py
+++ mainproject/entities/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Faculty(models.Model):
+    name = models.CharField(max_length=64)
+
 class Subject(models.Model):
     name = models.CharField(max_length=128)
-
-class Schedule(models.Model):
-    name = models.CharField(max_length=32)
+    faculty = Faculty()
 
 class FieldOfStudy(models.Model):
     name = models.CharField(max_length=64)
 
-class Faculty(models.Model):
-    name = models.CharField(max_length=64)
-
 class ScheduledSubject(models.Model):
     hours = models.DateTimeField()
+
+class Plan(models.Model):
+    title = models.CharField(max_length=32)
+    #schedule
