cd4f8a22cfab80aec5c363da685f5e81e38c2b79
AdamStan <adam15stan@gmail.com>
2018-12-29 01:19:23 +0100

    Ok, still in progress, but tomorrow I will finish algo

--- mainproject/entities/improvement.py
+++ mainproject/entities/improvement.py
@@ -20,7 +20,8 @@ class ImprovementManager:
         for p in plans:
             plan_ids_list.append(p.id)
             scheduled_subjects = subjects.filter(plan=p)
-            for i in range(1, self.day_of_week.size):  # 1 2 3 4 5 6 7
+            for i in range(1, self.day_of_week.size + 1):  # 1 2 3 4 5 6 7
+                #self.data[row][i-1] = []
                 self.data[row][i-1] = list(scheduled_subjects.filter(dayOfWeek=i))
             row = row + 1
 
@@ -28,16 +29,18 @@ class ImprovementManager:
         for r in rooms:
             rooms_list.append(r)
             scheduled_subjects = subjects.filter(room=r)
-            for i in range(1, self.day_of_week.size):  # 1 2 3 4 5 6 7
-                self.data[row][i - 1] = list(scheduled_subjects.filter(dayOfWeek=i))
+            for i in range(1, self.day_of_week.size + 1):  # 1 2 3 4 5 6 7
+                #self.data_rooms[row][i - 1] = []
+                self.data_rooms[row][i - 1] = list(scheduled_subjects.filter(dayOfWeek=i))
             row = row + 1
 
         row = 0
         for t in teachers:
             teachers_list.append(t)
             scheduled_subjects = subjects.filter(teacher=t)
-            for i in range(1, self.day_of_week.size):  # 1 2 3 4 5 6 7
-                self.data[row][i - 1] = list(scheduled_subjects.filter(dayOfWeek=i))
+            for i in range(1, self.day_of_week.size + 1):  # 1 2 3 4 5 6 7
+                #self.data_teachers[row][i - 1] = []
+                self.data_teachers[row][i - 1] = list(scheduled_subjects.filter(dayOfWeek=i))
             row = row + 1
 
 
@@ -48,7 +51,7 @@ class ImprovementManager:
     def generation(self, min_time_start=8, max_time_start=19):
         # IMPORTANT: which_plan_will_be_mutated, which_day, which_subject_index
         number_of_plans = self.plans_ids.size
-        which_plan_will_be_mutated = randint(0,number_of_plans)
+        which_plan_will_be_mutated = randint(0,number_of_plans-1)
         # for i in range(0,number_of_plans):
         buff = self.data[which_plan_will_be_mutated]
         print("+++ ONE PLAN +++")
@@ -63,8 +66,11 @@ class ImprovementManager:
         if buff[which_day] == 0:
             print("Empty day was chosen")
             return
-
-        which_subject_index = randint(0, len(buff[which_day]) - 1)
+        try:
+            which_subject_index = randint(0, len(buff[which_day]) - 1)
+        except ValueError:
+            # randint(0,0) but what if randint(0,-1)????
+            which_subject_index = 0
 
         subject_before = buff[which_day][which_subject_index]
         subject_after = deepcopy(subject_before)
@@ -76,15 +82,20 @@ class ImprovementManager:
 
         ImprovementManager.show_subject(subject_before)
         ImprovementManager.show_subject(subject_after)
-        ### START HERE NEXT DAY ###
+
         # checks that changes don't create any conflicts
-        what_return = self.check_subject_to_subjects(subject=subject_after, plan_one=buff[subject_after.dayOfWeek])
-        what_return = what_return and self.check_subject_to_teacher_time()
-        what_return = what_return and self.check_subject_to_rooms()
+        what_return = self.check_subject_to_subjects(subject=subject_after, plan_one=buff[subject_after.dayOfWeek - 1])
+        print(what_return)
+        what_return = what_return and self.check_subject_to_teacher(subject=subject_after, which_day=subject_after.dayOfWeek)
+        print(what_return)
+        what_return = what_return and self.check_subject_to_rooms(subject=subject_after, which_day=subject_after.dayOfWeek)
+        print(what_return)
 
         if what_return != True:
+            print("SOME CASE HAVE NOT PASSED")
             return
-
+        ### START HERE ###
+        return
         # counts value for old plan
         value_before = self.value_for_plan(plan=buff)
         # count value for new plan
@@ -102,13 +113,58 @@ class ImprovementManager:
         pass
 
     def check_subject_to_subjects(self, subject, plan_one):
-        pass
+        for sub in plan_one:
+            print("checking with: " + str(subject.dayOfWeek) + " " + str(sub.whenStart) + " " + str(sub.whenFinnish))
+            difference_starts = abs(subject.whenStart.hour - sub.whenStart.hour)
+            difference_ends = abs(subject.whenFinnish.hour - sub.whenFinnish.hour)
+            if (difference_starts + difference_ends) >= (subject.how_long + sub.how_long) or sub.id == subject.id:
+                continue
+            else:
+                return False
+        return True
 
-    def check_subject_to_teacher_time(self):
-        return False
+    def check_subject_to_teacher(self, subject, which_day):
+        teacher_index = self.teacher_in_array(subject.teacher)
+        teachers_week = self.data_teachers[teacher_index]
+        teacher_day = teachers_week[which_day - 1]
 
-    def check_subject_to_rooms(self):
-        return False
+        for sub in teacher_day:
+            print("checking with: " + str(subject.dayOfWeek) + " " + str(sub.whenStart) + " " + str(sub.whenFinnish))
+            difference_starts = abs(subject.whenStart.hour - sub.whenStart.hour)
+            difference_ends = abs(subject.whenFinnish.hour - sub.whenFinnish.hour)
+            if (difference_starts + difference_ends) >= (subject.how_long + sub.how_long) or sub.id == subject.id:
+                continue
+            else:
+                return False
+
+        return True
+
+    def check_subject_to_rooms(self, subject, which_day):
+        room_index = self.room_in_array(subject.room)
+        room_week = self.data_rooms[room_index]
+        room_day = room_week[which_day - 1]
+
+        for sub in room_day:
+            print("checking with: " + str(subject.dayOfWeek) + " " + str(sub.whenStart) + " " + str(sub.whenFinnish))
+            difference_starts = abs(subject.whenStart.hour - sub.whenStart.hour)
+            difference_ends = abs(subject.whenFinnish.hour - sub.whenFinnish.hour)
+            if (difference_starts + difference_ends) >= (subject.how_long + sub.how_long) or sub.id == subject.id:
+                continue
+            else:
+                return False
+        return True
+
+    def teacher_in_array(self, teacher):
+        for i in range(0, self.teachers.size):
+            if self.teachers[i] == teacher:
+                return i
+        return None
+
+    def room_in_array(self, room):
+        for i in range(0, self.rooms.size):
+            if self.rooms[i] == room:
+                return i
+        return None
 
     def make_improvement(self, number_of_generation):
         return False
